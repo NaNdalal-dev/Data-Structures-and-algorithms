ae539fb93e04291591cf166e79797dbb69b7583f
nandalal-learning-git <devnandalal95@gmail.com>
2021-01-22 16:43:06 +0530

    added recursive_binary_search.js

---
+++ recursive_binary_search.js
@@ -0,0 +1,35 @@
+step = 0
+flag = 0
+
+function RecursiveBinarySearch(arr,srh_val,low,high){
+
+	if( low <= high ){
+		mid = Math.floor((low+high)/2)
+		step+=1
+
+		if(srh_val == arr[mid]){
+			console.log('Element found at index : '+mid)
+			flag = 1
+			
+		}
+		else
+			if(srh_val < arr[mid]){
+				return RecursiveBinarySearch(arr,srh_val,low,mid-1)
+			}
+		else{
+			return RecursiveBinarySearch(arr,srh_val,mid+1,high)
+			
+		}
+	}
+	
+	if(flag == 0){
+	console.log('Element not found.')
+	}
+
+	console.log()
+	console.log('Length of Array:'+arr.length)
+	console.log('Steps taken:'+step)
+
+}
+
+RecursiveBinarySearch([1,2,3,4,5],90,0,4)
