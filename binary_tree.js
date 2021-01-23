a5b9b6ffa4dd11cc480c0cfb60ef0424a7e17375
NaNdalal-dev <dnandalal7@gmail.com>
2021-01-23 22:28:23 +0530

    added binary_tree.js file

---
+++ binary_tree.js
@@ -0,0 +1,66 @@
+class BinaryTree{
+	constructor(arr){
+		/*
+			Creating Instance Variables.
+		*/
+		this.arr = arr
+		this.length = arr.length
+
+	}
+
+	getParent(val){
+		//Formula to get parent of a leaf/child node
+		/*
+			parent = arr[i/2] #take floor value of i/2
+		*/
+		this.index = this.arr.indexOf(val)
+		return this.arr[Math.floor(this.index/2)]
+	}
+
+	getChildren(val){
+		//Formula to get children of a node/root
+		/*
+			left = 2*i+1
+			right = 2*i+2
+		*/
+		 this.index = this.arr.indexOf(val)
+		 this.left = this.arr[2*this.index+1]
+		 this.right = this.arr[2*this.index+2]
+		 return [this.left,this.right]
+	}
+
+	getLeftchild(val){
+		return	this.getChildren(val)[0]
+	}
+
+	getRightChild(val){
+		return this.getChildren(val)[1]
+	}
+
+	getHeight(){
+		//Formula to get Height of a binary tree
+		/*
+			floor(log(length of array)) #base = 2
+		*/
+		return Math.floor(Math.log2(this.length))
+	}
+
+	isFull(){
+		//Formula to know binary full tree 
+		/*
+			height = (2^(h+1))-1
+		*/
+		if(this.length == Math.pow(2,this.getHeight()+1)-1){
+			return true
+		}
+		return false
+	}
+}
+
+bt =new BinaryTree([1,2,3,4,5,6,7])
+
+//console.log(bt.getLeftchild(1))
+//console.log(bt.getRightChild(1))
+
+//console.log(bt.getHeight())
+console.log(bt.isFull())
